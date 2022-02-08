3e361cbb4d98d977dabe6eb9f9e15991dd6b62e6
mchatzinikolaou <mchatzinikolaouece@gmail.com>
2022-02-09 00:12:35 +0200

    some more stuff

--- Euler21.py
+++ Euler21.py
@@ -1,16 +1,26 @@
 import math
 divisorList= {}
-for num in range(1,1000+1):
+for num in range(2,10000+1):
     divisors=[]
-    for i in range(1,int(math.sqrt(num))+1):
+    for i in range(2,int(math.sqrt(num))+1):
         if num%i==0:
             divisors.append(i)
-            if(i**2!=num):
+            if i**2!=num:
                 divisors.append(num//i)
+    # +1 because we skipped division
+    # with 1 since it also adds n itself
+    divisorList[num]=sum(divisors)+1
 
-    divisorList[num]=sum(divisors)
 
-print(divisorList)
+amicableNumbers=[]
+for i in divisorList.keys():
+    if i not in amicableNumbers:
+        if divisorList[i] in divisorList.keys():
+                                                  #no such thing as
+                                                  #a self-amicable
+                                                  #number
+            if divisorList[divisorList[i]]==i and i!=divisorList[i]:
+                amicableNumbers.append(i)
+                amicableNumbers.append(divisorList[i])
 
-for divisor in divisorList.keys():
-    print(divisor)
+print(sum(amicableNumbers))
